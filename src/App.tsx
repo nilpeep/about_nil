16d1620e09dea87d38fd6c0617bdcaa5d8d0a4ea
nilpeep <niluferk038@gmail.com>
2024-05-20 20:53:32 +0300

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,65 @@
 import React, { useRef } from 'react'
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
 import cat from "./cat.gif"
+import satellite from './assets/satellite4.svg'
+import earth from './assets/earth.svg'
+import cloud from './assets/cloud.svg'
+import sky from './assets/sky.png'
+import Navbar from './components/navbar'
 
 // Little helpers ...
 const url = (name: string, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
-
+const links = ['about','contact','projects']
 export default function App() {
   const parallax = useRef<IParallax>(null!)
   return (
     <div style={{ width: '100%', height: '100%', background: '#253237' }}>
+      <div className="flex bg-[#6B66DF] w-100 h-20 fixed">
+      {links.map((item, key) => {
+        return(<div key={key}>{item}</div>)
+      })}
+    </div>
       <Parallax ref={parallax} pages={3}>
         <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
         <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
-
         <ParallaxLayer
           offset={0}
           speed={0}
           factor={3}
           style={{
-            backgroundImage: url('stars', true),
+            backgroundImage: `url(${sky})`,
             backgroundSize: 'cover',
           }}
         />
-
         <ParallaxLayer offset={0.5} speed={1} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+          <img src={satellite} style={{ width: '15%', marginLeft: '70%',marginBottom:'70%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+          <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <img src={cloud} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+          <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+          <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+          <img src={cloud} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+          <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+          <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+          <img src={cloud} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+          <img src={cloud} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+          <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+          <img src={cloud} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -63,8 +71,9 @@ export default function App() {
             justifyContent: 'center',
             pointerEvents: 'none',
           }}>
-          <img src={url('earth')} style={{ width: '60%' }} />
+          <img src={earth} style={{ width: '60%' }} />
         </ParallaxLayer>
+        
 
         <ParallaxLayer
           offset={1}
@@ -85,7 +94,7 @@ export default function App() {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <img src={url('server')} style={{ width: '20%' }} />
+          <h1>Hi Its nilufer!</h1>
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -98,6 +107,7 @@ export default function App() {
             justifyContent: 'center',
           }}>
           <img src={url('bash')} style={{ width: '40%' }} />
+          
         </ParallaxLayer>
         <ParallaxLayer
           offset={0}
@@ -108,7 +118,8 @@ export default function App() {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <img src={cat} style={{ width: '10%',margin:'50%',marginBottom:'80%' }} />
+          <img src={cat} style={{ width: '10%',margin:'50%',marginBottom:'81%' }} />
+          
         </ParallaxLayer>
 
         <ParallaxLayer
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+
+const links = ["about", "projects", "contact me"];
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex bg-[#6B66DF] w-100 h-20 fixed">
+      {links.map((item, key) => {
+        return(<div key={key}>{item}</div>)
+      })}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
