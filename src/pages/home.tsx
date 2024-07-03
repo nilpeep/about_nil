c9408968348766b9508132725ffd0bfea9154f9f
Nilufer Kaplan <121940876+nilpeep@users.noreply.github.com>
2024-07-03 05:12:38 +0300

    Update home.tsx
    navbar deployment bug

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/navbar/navbar";
+import Navbar from "../components/Navbar/navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 
