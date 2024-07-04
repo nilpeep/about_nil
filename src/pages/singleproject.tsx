fd22f62589b876c928ee2e924b38bacdc69ff7eb
nilpeep <niluferk038@gmail.com>
2024-07-04 16:33:02 +0300

    deploy

--- src/pages/singleproject.tsx
+++ src/pages/singleproject.tsx
@@ -31,7 +31,7 @@ const SingleProject: React.FC<ProjectProps> = () => {
           <div className="flex items-center mb-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-secondary mr-2"
+              className="h-6 w-6 text-highlight mr-2"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
