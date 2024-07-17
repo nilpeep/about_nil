9a15f04eaf9c5d31a4ebb2530619109d6aeb87a9
nilpeep <niluferk038@gmail.com>
2024-07-17 16:00:18 +0300

    blog img

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import SkeletonLoader from "../components/SkeletonLoading";
 import ErrorPage from "../components/ErrorPage";
 
@@ -18,23 +18,18 @@ const Blog = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const imgRef = useRef(null);
 
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const cachedArticles = localStorage.getItem("articles");
-        if (cachedArticles) {
-          setArticles(JSON.parse(cachedArticles));
-          setLoading(false);
-        } else {
-          const response = await fetch(
-            "https://v1.nocodeapi.com/nilpeep/medium/JXAIiQHRzjsIXvpg"
-          );
-          const data: Article[] = await response.json();
-          setArticles(data);
-          localStorage.setItem("articles", JSON.stringify(data));
-          setLoading(false);
-        }
+        const response = await fetch(
+          "https://v1.nocodeapi.com/nilpeep/medium/JXAIiQHRzjsIXvpg"
+        );
+        const data: Article[] = await response.json();
+        setArticles(data);
+        localStorage.setItem("articles", JSON.stringify(data));
+        setLoading(false);
       } catch (err) {
         console.log(err);
         if (err instanceof Error) {
@@ -63,9 +58,19 @@ const Blog = () => {
         {articles.map((article, index) => (
           <div
             onClick={() => handleArticleClick(article)}
-            className="cursor-pointer p-2 rounded-lg relative"
+            className="cursor-pointer p-2 rounded-lg relative flex items-center gap-5"
             key={index}
           >
+            {article.content.includes("img") ? (
+              <img
+                ref={imgRef}
+                className="h-20 w-40 object-cover rounded-lg"
+                src={article.content.match(/src="([^"]*)"/)[1]}
+                alt="article"
+              />
+            ) : (
+              <div className="w-full h-60 bg-gray-300 rounded-lg"></div>
+            )}
             <h2 className="text-highlight text-3xl hover-effect-highlight">
               {article.title}
             </h2>
